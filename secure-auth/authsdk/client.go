888ee40e970d52d00d7a59ae40a70f05b81e8b3c
dwayne g <dwayneg1130@gmail.com>
2025-10-06 10:45:25 -0700

    models update in authsdk pack

--- secure-auth/authsdk/client.go
+++ secure-auth/authsdk/client.go
@@ -10,13 +10,13 @@ import (
 	"github.com/dvg1130/Portfolio/secure-auth/models"
 )
 
-type Client struct {
+type ClientIn struct {
 	BaseURL    string
 	HTTPClient *http.Client
 }
 
-func NewClient(baseURL string) *Client {
-	return &Client{
+func NewClient(baseURL string) *ClientIn {
+	return &ClientIn{
 		BaseURL: baseURL,
 		HTTPClient: &http.Client{
 			Timeout: 10 * time.Second,
@@ -24,7 +24,7 @@ func NewClient(baseURL string) *Client {
 	}
 }
 
-func (c *Client) Login(username, password string) (*models.LoginResponse, error) {
+func (c *ClientIn) Login(username, password string) (*models.LoginResponse, error) {
 	payload := map[string]string{
 		"username": username,
 		"password": password,
